d1bfd364e8c35cf3e80144d688933638408735b1
Chanwoong Kim <chanwoong@chanwoong.kim>
2019-10-11 01:14:27 +0900

    Add TestCase that can use index notation

--- JumjaizeTest/JumjaizeTests.cs
+++ JumjaizeTest/JumjaizeTests.cs
@@ -12,6 +12,14 @@ namespace JumjaizeTest
             Assert.AreEqual(expected, new Jumjaize.Jumjaize().ToJumja(testStr));
         }
 
+        [TestCase("아버지", "1-2-6,4-5,2-3-4,4-6,1-3-5")]
+        public void ToJumjaTestWithIndexNotaion(string testStr, string expectedIndexNotation)
+        {
+            string expected = string.Join(string.Empty, Braille.CreateBraillesFromMultipleIndexNotation(expectedIndexNotation).Select(x => x.ToString()));
+            string actual = new Jumjaize.Jumjaize().ToJumja(testStr);
+            Assert.AreEqual(expected, actual);
+        }
+
         [TestCase("한글 점자", "⠚⠣⠒⠈⠪⠂ ⠨⠎⠢⠨⠣")]
         public void ToJumjaDirectly(string testStr, string expected)
         {
