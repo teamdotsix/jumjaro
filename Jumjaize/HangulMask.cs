d2db9eb0ccc5a3ffc64e750315eb82761539bda8
Chanwoong Kim <me@chanwoong.kim>
2019-10-14 01:40:44 +0900

    한글의 초,중,종성 마스킹용 클래스 HangulMask 추가

---
+++ Jumjaize/HangulMask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jumjaize
+{
+    public class HangulMask
+    {
+        private readonly char _onset;
+        private readonly char _nucleus;
+        private readonly char _coda;
+
+        public bool HasOnset => _onset != default(char);
+        public bool HasNucleus=> _onset != default(char);
+        public bool HasCoda => _onset != default(char);
+
+        public HangulMask(char onset = default, char nucleus = default, char coda = default)
+        {
+            _onset = onset;
+            _nucleus = nucleus;
+            _coda = coda;
+        }
+
+        public static bool operator &(HangulMask mask, char hangul)
+        {
+            var syllables = new Hangul().Syllabification(hangul);
+            var onset = syllables[0];
+            var nucleus = syllables[1];
+            var coda = syllables[2];
+
+            bool masked = true;
+            if(mask._onset != default(char))
+            {
+                masked &= mask._onset == onset;
+            }
+            if(mask._nucleus != default(char))
+            {
+                masked &= mask._nucleus == nucleus;
+            }
+            if(mask._coda != default(char))
+            {
+                masked &= mask._coda == coda;
+            }
+            return masked;
+        }
+
+    }
+}
---
+++ JumjaizeTest/HangulMaskTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Jumjaize;
+
+namespace JumjaizeTest
+{
+    class HangulMaskTests
+    {
+        [Test]
+        public void ANDOperatorTests()
+        {
+            Assert.AreEqual(true, new HangulMask('ㄱ', default, default) & '가');
+            Assert.AreEqual(true, new HangulMask('ㄱ', default, default) & '각');
+            Assert.AreEqual(true, new HangulMask('ㄱ', default, default) & '간');
+            Assert.AreEqual(false, new HangulMask('ㄱ', default, default) & '나');
+            Assert.AreEqual(false, new HangulMask('ㄱ', default, default) & '낙');
+            Assert.AreEqual(false , new HangulMask('ㄱ', default, default) & '난');
+
+            Assert.AreEqual(true, new HangulMask(default, 'ㅏ', default) & '나');
+            Assert.AreEqual(true, new HangulMask(default, 'ㅏ', default) & '난');
+            Assert.AreEqual(true, new HangulMask(default, 'ㅏ', default) & '아');
+            Assert.AreEqual(false, new HangulMask(default, 'ㅏ', default) & '어');
+            Assert.AreEqual(false, new HangulMask(default, 'ㅏ', default) & '왜');
+            Assert.AreEqual(false, new HangulMask(default, 'ㅏ', default) & '뷁');
+
+            Assert.AreEqual(true, new HangulMask(default, default, 'ㄴ') & '건');
+            Assert.AreEqual(true, new HangulMask(default, default, 'ㄴ') & '는');
+            Assert.AreEqual(true, new HangulMask(default, default, 'ㄴ') & '돈');
+            Assert.AreEqual(false, new HangulMask(default, default, 'ㄴ') & '핳');
+            Assert.AreEqual(false, new HangulMask(default, default, 'ㄴ') & '맥');
+            Assert.AreEqual(false, new HangulMask(default, default, 'ㄴ') & '누');
+
+            Assert.AreEqual(true, new HangulMask(default, 'ㅚ', 'ㄴ') & '왼');
+            Assert.AreEqual(true, new HangulMask(default, 'ㅚ', 'ㄴ') & '된');
+            Assert.AreEqual(true, new HangulMask(default, 'ㅚ', 'ㄴ') & '뵌');
+            Assert.AreEqual(false, new HangulMask(default, 'ㅚ', 'ㄴ') & '닳');
+            Assert.AreEqual(false, new HangulMask(default, 'ㅚ', 'ㄴ') & '용');
+            Assert.AreEqual(false, new HangulMask(default, 'ㅚ', 'ㄴ') & '됀');
+        }
+    }
+}
