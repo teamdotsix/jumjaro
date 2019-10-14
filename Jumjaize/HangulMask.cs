37cda6642994021501da85ddbfcbb1b71d991023
Chanwoong Kim <me@chanwoong.kim>
2019-10-14 23:49:52 +0900

    HangulMask가 쌍자음 종성에도 대응하도록 한다
    - IsMatch 함수를 추가하고 operator & 함수에서 IsMatch 함수를 불러다 쓰도록 함
    - DissembleCoda 함수를 추가하여 종성이 쌍자음일 경우 2개의 자음으로 분리할 수 있도록 한다

--- Jumjaize/HangulMask.cs
+++ Jumjaize/HangulMask.cs
@@ -13,6 +13,21 @@ namespace Jumjaize
         public bool HasOnset => _onset != default(char);
         public bool HasNucleus=> _onset != default(char);
         public bool HasCoda => _onset != default(char);
+        private static readonly Dictionary<char, char[]> _doubleCodaMap = new Dictionary<char, char[]>()
+        {
+            {'ㄲ', new[] {'ㄱ', 'ㄱ'}},
+            {'ㄳ', new[] {'ㄱ', 'ㅅ'}},
+            {'ㄵ', new[] {'ㄴ', 'ㅈ'}},
+            {'ㄶ', new[] {'ㄴ', 'ㅎ'}},
+            {'ㄺ', new[] {'ㄹ', 'ㄱ'}},
+            {'ㄻ', new[] {'ㄹ', 'ㅁ'}},
+            {'ㄼ', new[] {'ㄹ', 'ㅂ'}},
+            {'ㄽ', new[] {'ㄹ', 'ㅅ'}},
+            {'ㄾ', new[] {'ㄹ', 'ㅌ'}},
+            {'ㄿ', new[] {'ㄹ', 'ㅍ'}},
+            {'ㅀ', new[] {'ㄹ', 'ㅎ'}},
+            {'ㅄ', new[] {'ㅂ', 'ㅅ'}},
+        };
 
         public HangulMask(char onset = default, char nucleus = default, char coda = default)
         {
@@ -23,25 +38,66 @@ namespace Jumjaize
 
         public static bool operator &(HangulMask mask, char hangul)
         {
-            var syllables = new Hangul().Syllabification(hangul);
+            return mask.IsMatch(hangul);
+        }
+
+        public bool IsMatch(char hangulLetter)
+        {
+            var syllables = new Hangul().Syllabification(hangulLetter);
             var onset = syllables[0];
             var nucleus = syllables[1];
             var coda = syllables[2];
 
-            bool masked = true;
-            if(mask._onset != default(char))
+            bool matched = true;
+            if(_onset != default(char))
             {
-                masked &= mask._onset == onset;
+                matched &= _onset == onset;
             }
-            if(mask._nucleus != default(char))
+            if(_nucleus != default(char))
             {
-                masked &= mask._nucleus == nucleus;
+                matched &= _nucleus == nucleus;
             }
-            if(mask._coda != default(char))
+            if(_coda != default(char))
             {
-                masked &= mask._coda == coda;
+                // 제15항: 글자 속에 모음으로 시작하는 약자가 포함되어 있을 때에는 해당 약자를 이용하여 적는다.
+
+                // NOTE: 그러므로 쌍모음을 사용중일 경우에는 모음을 분리하여 약자를 체크한다.
+                var codas = DissembleCoda(coda);
+                matched &= _coda == codas[0];
             }
-            return masked;
+            return matched;
+        }
+
+        public bool IsMatch(char onset, char nucleus, char coda)
+        {
+            bool matched = true;
+            if(_onset != default(char))
+            {
+                matched &= _onset == onset;
+            }
+            if(_nucleus != default(char))
+            {
+                matched &= _nucleus == nucleus;
+            }
+            if(_coda != default(char))
+            {
+                // 제15항: 글자 속에 모음으로 시작하는 약자가 포함되어 있을 때에는 해당 약자를 이용하여 적는다.
+
+                // NOTE: 그러므로 쌍모음을 사용중일 경우에는 모음을 분리하여 약자를 체크한다.
+                var codas = DissembleCoda(coda);
+                matched &= _coda == codas[0];
+            }
+            return matched;
+        }
+
+        private static char[] DissembleCoda(char coda)
+        {
+            if (_doubleCodaMap.TryGetValue(coda, out var codas))
+            {
+                return codas;
+            }
+            return new [] {coda};
+
         }
 
     }
