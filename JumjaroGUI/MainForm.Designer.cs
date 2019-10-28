51f1982ce4c0ea30791b014af0e328bc86fdad24
Chanwoong Kim <me@chanwoong.kim>
2019-10-28 22:32:52 +0900

    GUI 기초 구현

---
+++ JumjaroGUI/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace JumjaroGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.translateButton = new System.Windows.Forms.Button();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(29, 74);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(319, 287);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(420, 74);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(319, 287);
+            this.outputTextBox.TabIndex = 0;
+            // 
+            // translateButton
+            // 
+            this.translateButton.Location = new System.Drawing.Point(420, 23);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(94, 29);
+            this.translateButton.TabIndex = 1;
+            this.translateButton.Text = "translate";
+            this.translateButton.UseVisualStyleBackColor = true;
+            this.translateButton.Click += new System.EventHandler(this.translateButton_Click);
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(791, 435);
+            this.Controls.Add(this.translateButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Name = "MainForm";
+            this.Text = "점자로";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button translateButton;
+    }
+}
+
---
+++ JumjaroGUI/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Jumjaro;
+
+namespace JumjaroGUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void translateButton_Click(object sender, EventArgs e)
+        {
+            var jumjaro = new Jumjaro.Jumjaro();
+            outputTextBox.Text = BrailleASCII.FromUnicode(jumjaro.ToJumja(inputTextBox.Text));
+        }
+    }
+}
