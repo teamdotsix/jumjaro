1e97132a660c93a023fe7fd68c2da1df4724af96
Chanwoong Kim <me@chanwoong.kim>
2019-10-29 22:59:58 +0900

    Braille ASCII 변환에 매핑 안 된 문자가 있을 시 발생하던 오류 수정
    Braiile Unicode <-> ASCII 매핑에 해당되지 않는 문자는 변환하지 않고 해당
    문자를 그대로 사용하는 방식으로 수정함

--- Jumjaro/BrailleASCII.cs
+++ Jumjaro/BrailleASCII.cs
@@ -11,15 +11,35 @@ namespace Jumjaro
         private static readonly string _brailleASCII = " a1b'k2l@cif/msp\"e3h9o6r^djg>ntq,*5<-u8v.%[$+x!&;:4\\0z7(_?w]#y)=";
         private static readonly string _brailleASCIItoUnicode = "⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿";
 
+        private static char TryGetUnicode(char ch)
+        {
+            var index = _brailleASCII.IndexOf(ch);
+            if (index != -1)
+            {
+                return _brailleASCIItoUnicode[index];
+            }
+            return ch;
+        }
+
         public static string ToUnicode(string brailleASCII)
         {
-            brailleASCII = brailleASCII.ToLower();
-            return new string(brailleASCII.Select(x => _brailleASCIItoUnicode[_brailleASCII.IndexOf(x)]).ToArray());
+            brailleASCII = brailleASCII.ToLower().Replace(' ', '⠀');
+            return new string(brailleASCII.Select(TryGetUnicode).ToArray());
+        }
+
+        private static char TryGetASCII(char ch)
+        {
+            var index = _brailleASCIItoUnicode.IndexOf(ch);
+            if (index != -1)
+            {
+                return _brailleASCII[index];
+            }
+            return ch;
         }
 
         public static string FromUnicode(string brailleUnicode)
         {
-            return new string(brailleUnicode.Select(x => _brailleASCII[_brailleASCIItoUnicode.IndexOf(x)]).ToArray());
+            return new string(brailleUnicode.Select(TryGetASCII).ToArray());
         }
     }
 }
