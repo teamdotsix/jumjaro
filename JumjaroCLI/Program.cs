c87255fdcf15b2907f530b78a34f91ce17f56c86
Chanwoong Kim <me@chanwoong.kim>
2019-11-04 01:14:56 +0900

    기본적인 기능의 CLI 구현

---
+++ JumjaroCLI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Jumjaro;
+using CommandLine;
+
+namespace JumjaroCLI
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option(SetName="OutputFormat", Default = true, HelpText = "결과를 유니코드 점자로 출력합니다.")]
+            public bool Unicode { get; set; }
+
+            [Option(SetName = "OutputFormat", HelpText = "결과를 BRF 점자로 출력합니다.")]
+            public bool BRF { get; set; }
+
+            [Value(0, Required = true, HelpText = "점자로 변환할 문자열")]
+            public string inputText { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Unicode)
+                       {
+                           Console.WriteLine(new Jumjaro.Jumjaro().ToJumja(o.inputText));
+                       }
+                       else
+                       {
+                           Console.WriteLine(BrailleASCII.FromUnicode(new Jumjaro.Jumjaro().ToJumja(o.inputText)));
+                       }
+                   });
+        }
+    }
+}
